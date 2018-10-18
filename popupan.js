fa872cd1ac9f2811e86c349f561986d473e135c5
thanhvy95 <40714663+thanhvy95@users.noreply.github.com>
2018-10-18 23:12:19 +0700

    Add files via upload

--- popupan.js
+++ popupan.js
@@ -1,8 +1,8 @@
 
-	var url_popup = 'https://bit.ly/2tFYYPh';
+	var url_popup = 'https://duanvillasphuquoc.blogspot.com/';
 
       //<![CDATA[
-	  // var url_ppp = 'http://bit.ly/1KmPNYe';
+	  // var url_ppp = 'https://duanvillasphuquoc.blogspot.com/';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
